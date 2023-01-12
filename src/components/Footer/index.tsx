672c125b73163a78fbee28f279bfd7215a518b24
José Eduardo Ribeiro <eduardojerbr@gmail.com>
2023-01-12 12:08:09 -0300

    add links logos

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -18,17 +18,29 @@ const Footer = (props: Props) => {
         <span data-testid="link_home" className={`${first === "/" && "bold"}`}>
           <Link to={"/"}>Home</Link>
         </span>
-        <span data-testid="link_pessoa" className={`${first === "/pessoa" && "bold"}`}>
+        <span
+          data-testid="link_pessoa"
+          className={`${first === "/pessoa" && "bold"}`}
+        >
           <Link to={"/pessoa"}>Pessoa Usuária</Link>
         </span>
-        <span data-testid="link_pro" className={`${first === "/profissional" && "bold"}`}>
+        <span
+          data-testid="link_pro"
+          className={`${first === "/profissional" && "bold"}`}
+        >
           <Link to={"/profissional"}>Profissional</Link>
         </span>
       </div>
       <div className="logos">
-        <FacebookLogo size={32} />
-        <InstagramLogo size={32} />
-        <LinkedinLogo size={32} />
+        <a target={"_blank"} href="https://www.facebook.com/lacrei.saude">
+          <FacebookLogo size={32} />
+        </a>
+        <a target={"_blank"} href="https://www.instagram.com/lacrei.saude/">
+          <InstagramLogo size={32} />
+        </a>
+        <a target={"_blank"} href="https://www.linkedin.com/company/lacrei/">
+          <LinkedinLogo size={32} />
+        </a>
       </div>
       <p>Desafio Front-end Lacrei</p>
     </Wrapper>
